1b7e280be804aab0611e94ada5b608e4589d61ab
Elizabeth Medvedeva <115479462+ElizabethMedvedeva@users.noreply.github.com>
2024-08-02 17:24:40 +0000

    fix: fix negative value of n

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function reverse(n) {
     let result = 0;
+    if (n < 0){
+        n = n * -1
+    }
     while (n) {
         result = result * 10 + n % 10;
         n = Math.floor(n / 10);
